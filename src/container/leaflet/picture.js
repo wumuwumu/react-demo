723f54e7e438b82f1eb736b7618f048520974040
wumuwumu <2232728687@qq.com>
2019-08-07 18:21:09 +0800

    feat:图片显示修改

--- src/container/leaflet/picture.js
+++ src/container/leaflet/picture.js
@@ -16,19 +16,22 @@ export default class picture extends Component {
     };
     this.map = L.map('map', {
       minZoom: 1,
-      maxZoom: 4,
+      maxZoom: 6,
       center: [0, 0],
-      zoom: 1,
+      zoom: 5,
       crs: L.CRS.Simple
   });
   // dimensions of the image
-  var w = 2880,
-      h = 1800,
+  var w = 600,
+      h = 360,
       url = 'https://images2.alphacoders.com/727/72732.png';
   // calculate the edges of the image, in coordinate space
   var southWest = this.map.unproject([0, h], this.map.getMaxZoom()-1);
   var northEast = this.map.unproject([w, 0], this.map.getMaxZoom()-1);
+  // var bounds = [[0,-180], [90,180]]
+
   var bounds = new L.LatLngBounds(southWest, northEast);
+  console.log(bounds);
   // add the image overlay,
   // so that it covers the entire map
   L.imageOverlay(url, bounds).addTo(this.map);
