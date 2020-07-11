cb359377438572f570ed1155182252cf706be56b
wumu <2232728687@qq.com>
2020-07-11 11:26:38 +0800

    feat:添加svg的操作

---
+++ src/container/svg/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import Picture from "./Picture1";
+export default class index extends Component {
+  render() {
+    return (
+      <div>
+        <Picture></Picture>
+      </div>
+    );
+  }
+}
--- src/router/index.js
+++ src/router/index.js
@@ -6,12 +6,14 @@ import Page404 from "../container/common/Page404";
 import Base from "../container/base/Index";
 import EchaertPage from "../container/echart";
 import Calendar from "../container/calendar";
+import SVG from "../container/svg";
 let routers = (
   <Switch>
     <Route path="/base" component={Base} />
     <Route path="/leaflet" component={leaflet} />
     <Route path="/echart" component={EchaertPage} />
     <Route path="/calendar" component={Calendar} />
+    <Route path="/svg" component={SVG} />
     <Route component={Page404} />
   </Switch>
 );
